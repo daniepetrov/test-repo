6a61277f7c80294978a2815fea13a36a4dbf4f8f
hookydev <dapetrov0@gmail.com>
2021-06-13 22:00:42 +0300

    Added subtitle

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const SubTitle = styled('h2')`
+  font-size: 34px;
+  color: black;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <div>
+      <Title>My page</Title>
+      <SubTitle>Hello</SubTitle>
+    </div>
+  );
 }
